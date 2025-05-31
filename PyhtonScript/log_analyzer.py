9594ca7a6699923ca4b5f2dac7c8bd81a9953bbb
Yahia_ezzahri <yahiaezzahri@gmail.com>
2025-05-31 09:34:37 +0100

    Yahia is first commit

---
+++ PyhtonScript/log_analyzer.py
@@ -0,0 +1,22 @@
+def analyser_log(fichier_log, fichier_rapport):
+    nb_info = nb_warning = nb_error = 0
+
+    with open(fichier_log, 'r') as f:
+        for ligne in f:
+            if 'INFO' in ligne:
+                nb_info += 1
+            elif 'WARNING' in ligne:
+                nb_warning += 1
+            elif 'ERROR' in ligne:
+                nb_error += 1
+
+    with open(fichier_rapport, 'w') as f:
+        f.write(f"Statistiques des logs :\n")
+        f.write(f"INFO : {nb_info}\n")
+        f.write(f"WARNING : {nb_warning}\n")
+        f.write(f"ERROR : {nb_error}\n")
+
+    print("Rapport généré avec succès.")
+
+if __name__ == "__main__":
+    analyser_log("bt.log", "rapport_bt.txt")
